7feb46991d1926357c21b1a0b9bfbdda19184f3b
DorAttard <104999929+DorAttard@users.noreply.github.com>
2024-03-08 17:16:36 +0100

    final update to site

--- Gabrielle/landing page gab test/main.js
+++ Gabrielle/landing page gab test/main.js
@@ -1,5 +1,5 @@
 // counter
-var countDownDate = new Date("March 8, 2024 15:40:00").getTime();
+var countDownDate = new Date("May 23, 2024 19:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
