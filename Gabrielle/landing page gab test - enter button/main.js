5e1221b72d1d3abe2d5f1d60c0aada9b0c2c78cc
GGau09 <gabrielle.gauci.16@um.edu.mt>
2024-03-18 10:56:59 +0100

    Create main.js

---
+++ Gabrielle/landing page gab test - enter button/main.js
@@ -0,0 +1,32 @@
+// counter
+var countDownDate = new Date("March 8, 2024 15:40:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes, and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="counter"
+  document.getElementById("counter").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    var counterElement = document.getElementById("counter");
+    counterElement.innerHTML = "EXHIBITION OPEN";
+  
+    // Add a class to the counter element
+    counterElement.classList.add("exhibition-open");
+  }
+}, 1000);
