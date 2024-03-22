b4a8f618ba6cf9a3793f4d7ef1370155174f7733
DorAttard <104999929+DorAttard@users.noreply.github.com>
2024-03-22 14:21:17 +0100

    update homepage

--- Dorianne/Homepage/burgerMenu.js
+++ Dorianne/Homepage/burgerMenu.js
@@ -10,19 +10,22 @@ function closeNav() {
 
 //navbar scroll
 
-// window.addEventListener('scroll', function() {
-//   var navbar = document.querySelector('.navbar');
-//   var contactSection = document.querySelector('.contact');
+window.addEventListener('scroll', function() {
+  var navbar = document.querySelector('.navbar');
+  var contactSection = document.querySelector('.catalogue');
+  var logoSVG = document.getElementById('logo'); // Get the SVG element
 
-//   var contactSectionTop = contactSection.offsetTop;
-//   var contactSectionHeight = contactSection.offsetHeight;
-//   var scrollPosition = window.scrollY;
+  var contactSectionTop = contactSection.offsetTop;
+  var contactSectionHeight = contactSection.offsetHeight;
+  var scrollPosition = window.scrollY;
 
-//   if (scrollPosition >= contactSectionTop && scrollPosition < contactSectionTop + contactSectionHeight) {
-//       // navbar.style.backgroundColor = 'white';
-//       navbar.style.color = '#FFFBEA'; // Change text color if needed
-//   } else {
-//       // navbar.style.backgroundColor = '#029B83'; // Default color when not over contact section
-//       navbar.style.color = '#E01F26'; // Default text color when not over contact section
-//   }
-// });
+  if (scrollPosition >= contactSectionTop && scrollPosition < contactSectionTop + contactSectionHeight) {
+      navbar.style.color = '#FFFBEA'; // Change text color if needed
+      logoSVG.src = 'logo-w.png'; // Change the image file for the SVG
+  } else {
+      navbar.style.color = '#E01F26'; // Default text color when not over contact section
+      logoSVG.src = 'logo-r.png'; // Default image file for the SVG
+  }
+});
+
+
