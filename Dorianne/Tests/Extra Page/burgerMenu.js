d07be7a87474f5e0b36594920fe86b4dc44e0212
DorAttard <104999929+DorAttard@users.noreply.github.com>
2024-04-18 16:47:09 +0200

    individual page test

---
+++ Dorianne/Tests/Extra Page/burgerMenu.js
@@ -0,0 +1,44 @@
+/* Open menu */
+function openNav() {
+    document.getElementById("myNav").style.display = "block";
+  }
+
+/* Close menu */
+function closeNav() {
+    document.getElementById("myNav").style.display = "none";
+  }
+
+//navbar scroll
+
+window.addEventListener('scroll', function() {
+  var navbar = document.querySelector('.navbar');
+  var contactSection = document.querySelector('.catalogue');
+  var logoSVG = document.getElementById('logo'); // Get the SVG element
+
+  var contactSectionTop = contactSection.offsetTop;
+  var contactSectionHeight = contactSection.offsetHeight;
+  var scrollPosition = window.scrollY;
+
+  if (scrollPosition >= contactSectionTop && scrollPosition < contactSectionTop + contactSectionHeight) {
+      navbar.style.color = '#FFFBEA'; // Change text color if needed
+      logoSVG.src = 'logo-w.png'; // Change the image file for the SVG
+  } else {
+      navbar.style.color = '#E01F26'; // Default text color when not over contact section
+      logoSVG.src = 'logo-r.png'; // Default image file for the SVG
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var scrollArrow = document.getElementById('scrollArrow');
+  scrollArrow.addEventListener('click', function(event) {
+      event.preventDefault();
+      var target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+          target.scrollIntoView({
+              behavior: 'smooth'
+          });
+      }
+  });
+});
+
+
