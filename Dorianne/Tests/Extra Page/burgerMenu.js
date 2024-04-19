8c6355b227b3e4bdf3d5b9c456e70220e7310979
DorAttard <104999929+DorAttard@users.noreply.github.com>
2024-04-19 12:15:21 +0200

    separating javascript code for navbar change colour scroll

--- Dorianne/Tests/Extra Page/burgerMenu.js
+++ Dorianne/Tests/Extra Page/burgerMenu.js
@@ -8,26 +8,7 @@ function closeNav() {
     document.getElementById("myNav").style.display = "none";
   }
 
-//navbar scroll
-
-window.addEventListener('scroll', function() {
-  var navbar = document.querySelector('.navbar');
-  var contactSection = document.querySelector('.catalogue');
-  var logoSVG = document.getElementById('logo'); // Get the SVG element
-
-  var contactSectionTop = contactSection.offsetTop;
-  var contactSectionHeight = contactSection.offsetHeight;
-  var scrollPosition = window.scrollY;
-
-  if (scrollPosition >= contactSectionTop && scrollPosition < contactSectionTop + contactSectionHeight) {
-      navbar.style.color = '#FFFBEA'; // Change text color if needed
-      logoSVG.src = 'logo-w.png'; // Change the image file for the SVG
-  } else {
-      navbar.style.color = '#E01F26'; // Default text color when not over contact section
-      logoSVG.src = 'logo-r.png'; // Default image file for the SVG
-  }
-});
-
+// arrow animation
 document.addEventListener('DOMContentLoaded', function() {
   var scrollArrow = document.getElementById('scrollArrow');
   scrollArrow.addEventListener('click', function(event) {
