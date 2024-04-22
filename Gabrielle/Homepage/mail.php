334c5d4d84a0f44c217e29fb4b0cef0f40d096d5
GGau09 <gabrielle.gauci.16@um.edu.mt>
2024-04-22 12:50:12 +0200

    update

--- Gabrielle/Homepage/mail.php
+++ Gabrielle/Homepage/mail.php
@@ -1,20 +1,17 @@
 <?php
   // Get the form data
   $email = $_POST['email'];
-  $information = $_POST['information'];
+  $text2 = $_POST['text2'];
 
   // Set the email recipient and subject
   $to = 'gabrielle.gauci.16@um.edu.mt';
   $subject = 'Form submission';
 
   // Create the email message
-  $message = "Email address: $email\n";
-  $message.= "Comment: $information\n";
+  $message.= "Comment: $text2\n";
 
   // Send the email
   mail($to, $subject, $message);
 
-  // Redirect the user to a thank-you page
-  header('Location: contact_us.html');
   exit;
 ?>
