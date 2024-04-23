f69205b78bce994d7da9b19761301630cc2e7a9f
DorAttard <104999929+DorAttard@users.noreply.github.com>
2024-04-23 21:28:56 +0200

    more updates

--- java/main.js
+++ java/main.js
@@ -13,22 +13,12 @@ function closeNav() {
     navOverlay.style.display = 'none'; 
   }
 
-//arrow smooth scroll
-document.addEventListener('DOMContentLoaded', function() {
-  var scrollArrows = document.querySelectorAll('.scrollArrow'); // Select all elements with the class 'scrollArrow'
-  scrollArrows.forEach(function(scrollArrow) { // Iterate through each element
-      scrollArrow.addEventListener('click', function(event) {
-          event.preventDefault();
-          var target = document.querySelector(this.getAttribute('href')); // Use 'this' to refer to the current element in the loop
-          if (target) {
-              target.scrollIntoView({
-                  behavior: 'smooth'
-              });
-          }
-      });
-  });
-});
-
-
-
-
+  // Top scroll bar
+  window.onscroll = function() {myFunction()};
+  
+  function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+   }
