41906f256f3b676232bded915d7b1845f5f7d526
DorAttard <104999929+DorAttard@users.noreply.github.com>
2024-04-30 18:42:58 +0200

    fix page, placed project css in a new file

--- java/scroll.js
+++ java/scroll.js
@@ -1,5 +1,4 @@
-let paragraphs = [...document.querySelectorAll('p')];
-let spans =[];
+let paragraphs = document.querySelectorAll('.text-reveal p');
 
 paragraphs.forEach(paragraph => {
     let htmlString = '';
@@ -8,22 +7,22 @@ paragraphs.forEach(paragraph => {
         htmlString += `<span>${pArray[i]}</span>`;
     }
     paragraph.innerHTML = htmlString;
-})
+});
 
-spans = [...document.querySelectorAll('span')];
+let spans = document.querySelectorAll('.text-reveal span');
 
-function revealSpans(){
-    for(let i = 0; i < spans.length; i++){
-        if(spans[i].parentElement.getBoundingClientRect().top < window.innerHeight / 2){
-            let {left, top} = spans[i].getBoundingClientRect();
-            top = top - (window.innerHeight * .5); //the start of the effect
+function revealSpans() {
+    spans.forEach(span => {
+        if(span.getBoundingClientRect().top < window.innerHeight / 2){
+            let {left, top} = span.getBoundingClientRect();
+            top = top - (window.innerHeight * .4); //the start of the effect
             let opacityValue = 1 - ((top * .1) + (left * 0.001)) < 0.1 ? 0.1 : 1 - ((top * .01) + (left * 0.001)).toFixed(3);
             opacityValue = opacityValue > 1 ? 1 : opacityValue.toFixed(3);
-            spans[i].style.opacity = opacityValue;
+            span.style.opacity = opacityValue;
         }
-    }
+    });
 }
 
 window.addEventListener('scroll', () => {
     revealSpans()
-})
+});
