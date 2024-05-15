c07dfb56ba191b4f028128409c5921534e84f6eb
DorAttard <104999929+DorAttard@users.noreply.github.com>
2024-05-15 08:42:15 +0200

    sponsorts update

--- java/navbarScrollProject.js
+++ java/navbarScrollProject.js
@@ -1,19 +1,40 @@
 //navbar scroll
 
+// window.addEventListener('scroll', function() {
+//     var navbar = document.querySelector('.navbar');
+//     var contactSection = document.querySelector('.bodytext-container');
+//     var logoSVG = document.getElementById('logo');
+  
+//     var contactSectionTop = contactSection.offsetTop;
+//     var contactSectionHeight = contactSection.offsetHeight;
+//     var scrollPosition = window.scrollY;
+  
+//     if (scrollPosition >= contactSectionTop && scrollPosition < contactSectionTop + contactSectionHeight) {
+//         navbar.style.color = '#FFFBEA'; 
+//         logoSVG.src = 'assets/logo-w.png'; 
+//     } else {
+//         navbar.style.color = '#E01F26'; 
+//         logoSVG.src = 'assets/logo-r.png';
+//     }
+//   });
+
 window.addEventListener('scroll', function() {
     var navbar = document.querySelector('.navbar');
     var contactSection = document.querySelector('.bodytext-container');
     var logoSVG = document.getElementById('logo');
+    var bmenu = document.getElementById('bmenu'); // Get the bmenu element
   
     var contactSectionTop = contactSection.offsetTop;
     var contactSectionHeight = contactSection.offsetHeight;
     var scrollPosition = window.scrollY;
   
     if (scrollPosition >= contactSectionTop && scrollPosition < contactSectionTop + contactSectionHeight) {
-        navbar.style.color = '#FFFBEA'; 
-        logoSVG.src = 'assets/logo-w.png'; 
+        navbar.style.color = '#FFFBEA'; // Change text color if needed
+        logoSVG.src = 'assets/logo-w.png'; // Change the image file for the SVG
+        bmenu.classList.add('white-bmenu'); // Add white-bmenu class
     } else {
-        navbar.style.color = '#E01F26'; 
-        logoSVG.src = 'assets/logo-r.png';
+        navbar.style.color = '#E01F26'; // Default text color when not over contact section
+        logoSVG.src = 'assets/logo-r.png'; // Default image file for the SVG
+        bmenu.classList.remove('white-bmenu'); // Remove white-bmenu class
     }
-  });
+});
