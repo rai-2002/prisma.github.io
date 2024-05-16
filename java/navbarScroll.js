ba4c65e0995c0afe787b4cec737c968b6d03a9a6
DorAttard <104999929+DorAttard@users.noreply.github.com>
2024-05-16 07:12:25 +0200

    final updates

--- java/navbarScroll.js
+++ java/navbarScroll.js
@@ -1,14 +1,19 @@
 window.addEventListener('scroll', function() {
     var navbar = document.querySelector('.navbar');
     var contactSection = document.querySelector('.catalogue');
+    var classImageSection = document.querySelector('.classImage'); // Get the .classImage section
     var logoSVG = document.getElementById('logo');
     var bmenu = document.getElementById('bmenu'); // Get the bmenu element
   
     var contactSectionTop = contactSection.offsetTop;
     var contactSectionHeight = contactSection.offsetHeight;
+    var classImageSectionTop = classImageSection.offsetTop;
+    var classImageSectionHeight = classImageSection.offsetHeight;
     var scrollPosition = window.scrollY;
-  
-    if (scrollPosition >= contactSectionTop && scrollPosition < contactSectionTop + contactSectionHeight) {
+
+    // Check if scroll position is within the bounds of contactSection or classImageSection
+    if ((scrollPosition >= contactSectionTop && scrollPosition < contactSectionTop + contactSectionHeight) || 
+        (scrollPosition >= classImageSectionTop && scrollPosition < classImageSectionTop + classImageSectionHeight)) {
         navbar.style.color = '#FFFBEA'; // Change text color if needed
         logoSVG.src = 'assets/logo-w.png'; // Change the image file for the SVG
         bmenu.classList.add('white-bmenu'); // Add white-bmenu class
@@ -18,3 +23,4 @@ window.addEventListener('scroll', function() {
         bmenu.classList.remove('white-bmenu'); // Remove white-bmenu class
     }
 });
+
